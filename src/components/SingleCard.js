e264e758989e0423e75d19230d59e1dc978c6f27
Ahmet Ustun <austu@softserveinc.com>
2022-07-28 16:32:38 +0300

    commiting the whole project at once

---
+++ src/components/SingleCard.js
@@ -0,0 +1,25 @@
+import "./SingleCard.css";
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped && "flipped"}>
+        <img className="front" src={card.src} alt="Card Front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="Card Back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
